8195f31f3cb9c59c92f78d86b351db03e6ed4807
Eptick <redzicleon@gmail.com>
2015-07-21 22:41:42 +0200

    s

--- wp-config.php
+++ wp-config.php
@@ -14,26 +14,39 @@
  * @package WordPress
  */
 
+/*
+define('DB_NAME', 'c9');
+define('DB_USER', getenv('C9_USER'));
+define('DB_PASSWORD', '');
+define('DB_HOST', getenv('IP'));
+define('WP_SITEURL', 'http://' . getenv('C9_PROJECT') . '.' . getenv('C9_USER') . '.c9.io');
+define('DB_NAME', 'wordpress');
+*/
+
+
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+define('DB_NAME', 'c9');
+define('DB_USER', getenv('C9_USER'));
+define('DB_PASSWORD', '');
+define('DB_HOST', getenv('IP'));
+define('WP_SITEURL', 'https://' . getenv('C9_PROJECT') . '.' . getenv('C9_USER') . '.c9.io');
 define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
-
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
-define( 'WP_SITEURL', 'http://93.138.1.200/wp' );
-define( 'WP_HOME', 'http://93.138.1.200/wp' );
 
 
 /**#@+
